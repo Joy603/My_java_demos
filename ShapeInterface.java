013b215aca060fdf3a6214443c3d041eadf33587
Joy603 <Joy603@users.noreply.github.com>
2016-07-22 22:47:02 -0400

    Add files via upload

---
+++ ShapeInterface.java
@@ -0,0 +1,10 @@
+package JavaShapesDemo;
+
+/**
+ *
+ * This is the Shape interface for this test
+ */
+public interface ShapeInterface {
+    public double getArea();
+    public double getPerimeter();
+}
